d867abb7ca14d39888a51c02659f5b91a4c8fc16
Xmuhia <109690494+Xmuhia@users.noreply.github.com>
2022-11-30 23:46:03 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { NavBar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='app' >
+        <div className='navbar'>
+            <NavBar />
+        </div>
+        <div className='main'>
+            <Layout>
+              <Switch>
+                <Route exact path='/' >
+                  <Homepage />
+                </Route>
+                <Route exact path='/exchanges' >
+                  <Exchanges />
+                </Route>
+                <Route exact path='/cryptocurrencies' >
+                  <Cryptocurrencies />
+                </Route>
+                <Route exact path='/crypto/:coinId' >
+                  <CryptoDetails />
+                </Route>
+                <Route exact path='/News' >
+                  <News />
+                </Route>
+              </Switch>
+            </Layout>
+        </div>
+        <div className='footer'>
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
