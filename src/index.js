d4a1400d4b841b9a91b08d1da466313bcfc17b8e
muhia <109690494+Xmuhia@users.noreply.github.com>
2022-12-05 18:52:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import Store from './App/store';
 
 ReactDOM.render(
     <Router>
-        <App /> 
+        <Provider store={store}>
+            <App /> 
+        </Provider>
     </Router>, document.getElementById('root'));
