c11ed3072a126c4a72e3ba2d551767747c836cec
muhia <109690494+Xmuhia@users.noreply.github.com>
2022-12-05 18:53:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from 'antd';
 
 import { NavBar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components';
-
 import './App.css';
 
 const App = () => {
   return (
     <div className='app' >
-        <div className='navbar'>
-            <NavBar />
-        </div>
-        <div className='main'>
-            <Layout>
-              <Switch>
-                <Route exact path='/' >
-                  <Homepage />
-                </Route>
-                <Route exact path='/exchanges' >
-                  <Exchanges />
-                </Route>
-                <Route exact path='/cryptocurrencies' >
-                  <Cryptocurrencies />
-                </Route>
-                <Route exact path='/crypto/:coinId' >
-                  <CryptoDetails />
-                </Route>
-                <Route exact path='/News' >
-                  <News />
-                </Route>
-              </Switch>
-            </Layout>
-        </div>
-        <div className='footer'>
+      <div className='navbar'>
+        <NavBar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route path='/news' element={<News />} />
+              <Route path='/exchanges' element={<Exchanges />} />
+              <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+              <Route path='*' element={<Homepage />} />
+            </Routes>
+          </div>
+        </Layout>
 
+        <div className='footer'>
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }} >
+            Charge Flow <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
         </div>
+      </div>
     </div>
   );
 }
